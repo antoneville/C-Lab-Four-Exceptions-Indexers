b59c7549bbff1655b8b76d2ef222242dffb2539d
antoneville <antoneville@hotmail.com>
2018-09-30 20:31:47 +0100

    Excersice One - Not working

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Maths
+{
+    class Calculator
+    {
+        static public float calcFloat(float numOne, float numTwo) 
+        {
+            if (numTwo == 0)
+            {
+                throw new ArgumentException();
+            } 
+            else
+            {
+                float divide = numOne/numTwo;
+                return divide;
+            }
+        }
+    }
+
+    class Test
+    {
+        void Main()
+        {   
+            String one = "2.4F";
+            String two = "3.2F";
+            Double.Parse(one);
+            Double.Parse(two);
+            Calculator cTwo = new Calculator();
+            cTwo.calcFloat(one, two);
+        }
+    }
+
+}
